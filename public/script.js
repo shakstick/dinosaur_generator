5f8ee8ed8cf7a46bd0d628b035a8438fd0378884
Shak Stick <sanshakurt@gmail.com>
2022-07-30 14:49:55 +0430

    complete the project

--- public/script.js
+++ public/script.js
@@ -1,14 +1,21 @@
-let din;
-
 const btn = document.getElementById("btn-gnr");
-const img = document.querySelector("img");
-const h1 = document.querySelector("h1");
+const dinoName = document.getElementById("dino-name");
+const copyIcon = document.getElementById("copy-icon");
+const copyText = document.getElementById("copy-text");
+const section = document.getElementById("section");
+
+// fetchDinoName();
+// fetchDinoImage();
 
 async function fetchDinoName() {
   await fetch("/dinosaur")
     .then((response) => response.json())
     .then((data) => {
-      h1.textContent = data[0].join(" ");
+      dinoName.textContent = data[0].join(" ");
+      try {
+        dinoName.classList.remove("hidden");
+        copyIcon.classList.remove("hidden");
+      } catch {}
     });
 }
 
@@ -16,9 +23,23 @@ async function fetchDinoImage() {
   await fetch("./image")
     .then((response) => response.json())
     .then((data) => {
+      try {
+        document.querySelector("img").remove();
+      } catch {}
+      const img = document.createElement("img");
       img.setAttribute("src", data.urls.small);
+      img.setAttribute("alt", "dinosaur image");
+      section.appendChild(img);
     });
 }
+
+copyIcon.addEventListener("click", () => {
+  navigator.clipboard.writeText(dinoName.textContent);
+  copyText.classList.add("active");
+  setTimeout(() => {
+    copyText.classList.remove("active");
+  }, 1500);
+});
 
 btn.addEventListener("click", () => {
   fetchDinoName();
